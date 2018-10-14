c45034f9aad6ecaaeec83ebb0815bf41c96ec884
masoud rafiei <mrafiei@student.concordia.ab.ca>
2018-10-14 17:57:45 -0600

    passPhrase generatation method

--- src/main/java/net/seleucus/wsp/server/commands/WSUserAdd.java
+++ src/main/java/net/seleucus/wsp/server/commands/WSUserAdd.java
@@ -33,7 +33,6 @@ public class WSUserAdd extends WSCommandOption {
 
 //            passSeq = myServer.readPasswordRequired("Enter the New User's Pass-Phrase");
             String username = myServer.readLineRequired("choose username(must be followed by 3 digits) ");
-            
 
 //            passPhraseInUse = myServer.getWSDatabase().passPhrases.isPassPhraseInUse(passSeq);
 //            if (passPhraseInUse == true) {
@@ -41,7 +40,7 @@ public class WSUserAdd extends WSCommandOption {
 //                myServer.println("WebSpa pass-phrases have to be unique for each user");
 //            }
             usernameIsValid = isUsernameValid(username);
-            System.out.println("usernameIsValid "+usernameIsValid);
+            System.out.println("usernameIsValid " + usernameIsValid);
             if (usernameIsValid == false) {
                 myServer.println("This username is already taken and in use by another user or it is not valid!");
                 myServer.println("WebSpa username have to be unique for each user");
@@ -51,8 +50,9 @@ public class WSUserAdd extends WSCommandOption {
             usernameSet = generateUsernameSet(usernameCount, username);
             CharSequence passPhrase = myServer.readPasswordRequired("choose pass phrase(including digits,uppercase and lower case) ");
             int pasCount = myServer.readLineRequiredInt("Enter the number of passwords you want ", 1, 100);
-            int lenght = myServer.readLineRequiredInt("Enter the lenght of passwords you want ", 6, 12);
-            passSet = generatePassphraseSet(pasCount, lenght);
+            //int lenght = myServer.readLineRequiredInt("Enter the lenght of passwords you want ", 6, 12);
+            //passSet = generatePassphraseSet(pasCount, lenght);
+            passSet = generatePassphraseSet(pasCount, passPhrase.toString());
 
         } while (!usernameIsValid);
 
@@ -122,6 +122,109 @@ public class WSUserAdd extends WSCommandOption {
         return passPhraseSet;
     }
 
+    private String[] generatePassphraseSet(int passPhrasecount, String userPassPhrase) {
+//        System.out.println("1");
+        int passIndex = (getIntSeed() % passPhrasecount);
+        String pattern = extractPattern(userPassPhrase);
+//        System.out.println("2");
+        String passPhraseSet[] = new String[passPhrasecount];
+        int tempDigit = 0;
+        int counter = 0;
+        int numberOfChanges = 3;
+        String tempPassPhrase = userPassPhrase;
+//        System.out.println("3");
+        while (counter < passPhrasecount) {
+//            System.out.println("tempPass bef: " + tempPassPhrase);
+            tempPassPhrase = changeUpperToLower(tempPassPhrase, pattern);
+//            System.out.println("tempPass changeUpperToLower: " + tempPassPhrase);
+
+//            System.out.println("4");
+            tempPassPhrase = changeLowerToUpper(userPassPhrase.length(), tempPassPhrase, pattern);
+//            System.out.println("tempPass changeLowerToUpper: " + tempPassPhrase);
+
+//            System.out.println("5");
+            tempPassPhrase = changeDigits(pattern, tempPassPhrase);
+//            System.out.println("6");
+
+            if (counter == 0) {
+                if (counter != passIndex) {
+                    passPhraseSet[counter] = tempPassPhrase;
+                } else {
+                    passPhraseSet[counter] = tempPassPhrase;
+                }
+                counter++;
+                System.out.println("tempPass" + (counter + 1) + ": " + passPhraseSet[counter]);
+
+            } else if (validateTempPassPhrase(passPhraseSet, tempPassPhrase,counter)) {
+                if (counter != passIndex) {
+                    passPhraseSet[counter] = tempPassPhrase;
+                } else {
+                    passPhraseSet[counter] = tempPassPhrase;
+                }
+                counter++;
+            }
+            tempPassPhrase = userPassPhrase;
+
+        }
+
+        return passPhraseSet;
+    }
+
+    private String changeLowerToUpper(int userPassPhraselength, String tempPassPhrase, String pattern) {
+        boolean properIndexOfUppercaseIsNotfound = true;
+
+        while (properIndexOfUppercaseIsNotfound) {
+            int newIndexOfUppercase = getIntSeed() % (userPassPhraselength);
+            if (Character.isLetter(tempPassPhrase.charAt(newIndexOfUppercase)) && Character.isLowerCase(tempPassPhrase.charAt(newIndexOfUppercase))) {
+                tempPassPhrase = tempPassPhrase.substring(0, newIndexOfUppercase) + tempPassPhrase.substring(newIndexOfUppercase, newIndexOfUppercase + 1).toUpperCase() + tempPassPhrase.substring(newIndexOfUppercase + 1);
+                properIndexOfUppercaseIsNotfound = false;
+//                System.out.println("4.2");
+
+            }
+//            System.out.println("4.3");
+
+        }
+        return tempPassPhrase;
+    }
+
+    private String changeUpperToLower(String tempPassPhrase, String pattern) {
+        int indexOfUppercase = pattern.indexOf("U");
+//        System.out.println("3.1-- " + indexOfUppercase);
+        int lastIndexOfUppercase = pattern.lastIndexOf("U");
+//        System.out.println("3.2-- " + lastIndexOfUppercase);
+        if (lastIndexOfUppercase == indexOfUppercase || lastIndexOfUppercase <= 0) {
+            tempPassPhrase = tempPassPhrase.substring(0, indexOfUppercase) + tempPassPhrase.substring(indexOfUppercase, indexOfUppercase + 1).toLowerCase() + tempPassPhrase.substring(indexOfUppercase + 1);
+        } else if ((getIntSeed() % 2) == 0) {
+            tempPassPhrase = tempPassPhrase.substring(0, indexOfUppercase) + tempPassPhrase.substring(indexOfUppercase, indexOfUppercase + 1).toLowerCase() + tempPassPhrase.substring(indexOfUppercase + 1);
+        } else {
+            tempPassPhrase = tempPassPhrase.substring(0, lastIndexOfUppercase) + tempPassPhrase.substring(lastIndexOfUppercase, lastIndexOfUppercase + 1).toLowerCase() + tempPassPhrase.substring(lastIndexOfUppercase + 1);
+        }
+        return tempPassPhrase;
+    }
+
+    private String changeDigits(String pattern, String tempPassPhrase) {
+        int countOfDigits = countOccurrence(pattern, 'D');
+        int indexOfchange = pattern.indexOf('D');
+
+        if (countOfDigits > 1) {
+            int digitChangeNo = getIntSeed() % (countOfDigits + 1);
+            String temp = pattern;
+            int tempIndexOfchange = 0;
+            for (int j = 1; j <= digitChangeNo; j++) {
+                if (temp.indexOf("D") >= 0) {
+                    tempIndexOfchange += (temp.indexOf("D") == 0 ? 1 : temp.indexOf("D"));
+                    temp = temp.substring(temp.indexOf("D") + 1);
+                }
+//                System.out.println("indexOfchange: " + tempIndexOfchange + "  --temp: " + temp+" digitChangeNo: "+digitChangeNo);
+            }
+            indexOfchange = (tempIndexOfchange > indexOfchange ? tempIndexOfchange : indexOfchange);
+        }
+
+        tempPassPhrase = tempPassPhrase.substring(0, indexOfchange) + (getIntSeed() % 10) + tempPassPhrase.substring(indexOfchange + 1);
+
+        return tempPassPhrase;
+    }
+
     private char getCharSeed() {
         Date dateobj = new Date();
 //        System.out.println("raw date: " + dateobj.getTime());
@@ -171,14 +274,64 @@ public class WSUserAdd extends WSCommandOption {
 
         return usernameSet;
     }
-    
-    private boolean isUsernameValid(String username){
-        boolean usernameValid=false;
-        
-        usernameValid = NumberUtils.isNumber(username.substring(username.length()-3));
-        
-        usernameValid=usernameValid && !myServer.getWSDatabase().users.isUsernameInUse(username);
+
+    private boolean isUsernameValid(String username) {
+        boolean usernameValid = false;
+
+        usernameValid = NumberUtils.isNumber(username.substring(username.length() - 3));
+
+        usernameValid = usernameValid && !myServer.getWSDatabase().users.isUsernameInUse(username);
 //        System.out.println("-----mia----- "+usernameValid);
         return usernameValid;
+    }
+
+    private boolean validateTempPassPhrase(String[] passPhraseSet, String tempPassPhrase, int setLenght) {
+        boolean isPassPhraseRedundant = false;
+
+//        if (passPhraseSet.length > 0) {
+        System.out.println("************** " + passPhraseSet[0]);
+        for (int i=0;i<setLenght;i++) {
+            
+            System.out.println("****22********** " + passPhraseSet[i]);
+            if (passPhraseSet[i].equals(tempPassPhrase)) {
+                isPassPhraseRedundant = true;
+                System.out.println("*- info: PassPhrase is Redundant");
+            }
+//            }
+        }
+        return isPassPhraseRedundant;
+    }
+
+    private String extractPattern(String expression) {
+
+        String extractedPattern = "";
+        char tempChar;
+        for (int i = 0; i < expression.length(); i++) {
+            tempChar = expression.charAt(i);
+            if (Character.isDigit(tempChar)) {
+                extractedPattern += "D";      //D for Digits
+            } else if (Character.isLowerCase(tempChar)) {
+                extractedPattern += "L";      // L for Lowercase
+            } else if (Character.isUpperCase(tempChar)) {
+                extractedPattern += "U";      // U for Uppercase
+            }
+
+        }
+
+//        System.out.println("-----mia----- "+usernameValid);
+        return extractedPattern;
+    }
+
+    private int countOccurrence(String expression, char chr) {
+
+        int occurrence = 0;
+        for (int i = 0; i < expression.length(); i++) {
+
+            if (expression.charAt(i) == chr) {
+                occurrence++;      //D for Digits
+            }
+        }
+//        System.out.println("-----mia----- " + occurrence);
+        return occurrence;
     }
 }
